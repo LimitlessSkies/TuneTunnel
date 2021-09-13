157b9761459abc0bbbe9b853b32032a4c68557d4
LimitlessSkies <chris.mcgrath11@gmail.com>
2021-09-13 10:01:14 -0600

    Tune tunnel setup

---
+++ app.js
@@ -0,0 +1,34 @@
+const API_SECRET;
+const API_KEY;
+const API_URL;
+
+
+function getAuth() {
+
+}
+
+function getArtists() {
+
+}
+
+function searchArtists(){
+
+}
+
+function createTopTen(){
+
+}
+
+function artistBranching(){
+
+}
+
+function fullArtistList(){
+
+}
+function getArtistSongs(){
+
+}
+function playlistSync(){
+
+}
